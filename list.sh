94624a2be6e53e872b4ca2a20699caf4f5109943
Mark Sarcevicz <>
2021-03-18 12:58:38 -0400

    updated

--- list.sh
+++ list.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 # merge, dev, alpha, staging, producton
-env=(merge dev alpha staging production)
+env=(merge production)
 version=${1:-1.38.0-SNAPSHOT}
 gsutil -q cp gs://deployment-tracking/endpoints.json .
+mergedate=""
+proddate=""
 for i in ${env[@]}; do
   input=$(jq '.data[] | select(.semVer | contains("'"$version"'")) | select(.env | contains("'"$i"'"))' endpoints.json | jq -s '.')
-  echo $input | jq -r .[0]
+  if [[ $(echo $input | jq -r .[0].env) == "merge" ]]; then
+    mergedate=$(echo $input | jq -r .[0].date)
+    A=$(echo ${mergedate} | awk '{print $1}')
+    printf "${version}: merge date: $A\n"
+  else
+    proddate=$(echo $input | jq -r .[0].date)
+    B=$(echo ${proddate} | awk '{print $1}' )
+    printf "${version}: production date: $B\n"
+  fi
 done
+printf "Number of days to Production: $(((`date -jf %Y-%m-%d $B +%s` - `date -jf %Y-%m-%d $A +%s`)/86400))\n"
